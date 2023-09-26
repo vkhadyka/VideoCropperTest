1e8b53b7e8608f63094e93b9b10177f215773e83
Vadim Khadyka <khadyka.vadim@gmail.com>
2023-09-26 10:01:40 +0300

    Initial Commit

---
+++ VideoCropperTest/VideoCropperTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCropperTestApp.swift
+//  VideoCropperTest
+//
+//  Created by Vadim Khadyka on 26.09.23.
+//
+
+import SwiftUI
+
+@main
+struct VideoCropperTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
